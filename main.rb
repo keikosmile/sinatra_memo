64c445b6463b37c7ca2a5713f7fa4d6dd365f832
keikosmile <69710419+keikosmile@users.noreply.github.com>
2021-07-28 17:40:27 +0900

    データベースをjsonファイルへ変更。URIを変更

--- main.rb
+++ main.rb
@@ -1,90 +1,81 @@
 require 'sinatra'
 # 再起動を不要にする
 require 'sinatra/reloader'
-
-class Memo
-  attr_accessor :title, :content, :date
-
-  def initialize(title, content, date)
-    @title = title
-    @content = content
-    @date = date
-  end
-end
-
-class MemoDatabase
-  attr_accessor :mda
-
-  def initialize
-    @mda = []
-  end
-
-  def get_memo(id)
-    @mda[id]
-  end
-
-  def insert(title, content)
-    memo = Memo.new(title, content, Time.now)
-    @mda.push(memo)
-  end
-
-  def edit(id, title, content)
-    @mda[id].title = title
-    @mda[id].content = content
-    @mda[id].date = Time.now
-  end
-
-  def delete(id)
-    @mda.delete_at(id)
-  end
-end
+# JSONモジュールを利用する
+require 'json'
 
 # グローバル変数
-$title = 'メモアプリ'
-# MemoDatabase をインスタン化して、アプリケーションのスコープで保存する
-set :md, MemoDatabase.new
+$application_title = 'メモアプリ'
+$database_file = './database.json'
 
-# get, post の処理は、リクエストスコープ
-# home表示
-get '/' do
-  erb :home
+# コンフィギュレーション: アプリケーションスコープ
+configure do
+  set :result_hash, []
 end
 
-# メモのnew表示
-get '/new' do
+# get, post の処理: リクエストスコープ
+# index表示
+get '/' do
+  # 初めて起動した時、result_hash = []
+  # データ操作した後、result_hash = [] or != []
+  if !settings.result_hash.empty?
+    # データベースファイルへ書き込む
+    File.open($database_file, "w") do |file|
+      JSON.dump(settings.result_hash, file)
+    end
+  end
+  # データベースファイルが空でなければ、それぞれのメモを表示する
+  # データベースファイルが空であれば、メモは1つも表示しない
+  unless FileTest.zero?($database_file)
+    # ファイルを開き、ファイルオブジェクトを生成する
+    File.open("database.json") do |file|
+      # JSON形式のファイルオブジェクトを、JSON形式の文字列に変換し、Rubyオブジェクト(ハッシュ）に変換する
+      # settingsヘルパーを利用し、リクエストスコープからアプリケーションスコープにアクセス
+      settings.result_hash = JSON.parse(file.read, symbolize_names: true)
+    end
+  end
+  erb :index
+end
+
+# メモの新規作成
+get '/memos/new' do
   erb :new
 end
 
-# new表示後、DBへinsertし、'/'へリダイレクト
-post '/new' do
-  # settingsヘルパーを利用し、リクエストスコープからアプリケーションスコープにアクセス
-  settings.md.insert(params[:title], params[:content])
+# new表示後、result_hash へinsertし、'/'へリダイレクト
+post '/memos/new' do
+  # 新しいハッシュを作成し、配列の最後へ挿入する
+  hash = {title: params[:title], body: params[:body]}
+  settings.result_hash.push(hash)
   redirect '/'
 end
 
-# 各メモのshow表示
-get '/memos/:id' do
-  @id = params[:id].to_i
-  erb :show
+# 各メモの detail表示
+get '/memos/:memo_id' do
+  @memo_id = params[:memo_id].to_i
+  erb :detail
 end
 
-# 各メモのshow表示後、DBを削除し、'/'へリダイレクト
-post '/memos/:id' do
-  @id = params[:id].to_i
-  settings.md.delete(@id)
+# 各メモの detail 表示後、result_hash から削除し、'/'へリダイレクト
+delete '/memos/:memo_id' do
+  @memo_id = params[:memo_id].to_i
+  # 配列から対象ハッシュを削除する
+  settings.result_hash.delete_at(@memo_id)
   redirect '/'
 end
 
-# 各メモのshow表示後、edit表示
-get '/memos/:id/edit' do
-  @id = params[:id].to_i
+# 各メモの detail 表示後、メモ編集用のHTMLフォームを表示
+get '/memos/:memo_id/edit' do
+  @memo_id = params[:memo_id].to_i
   erb :edit
 end
 
-# 各メモのedit表示で変更後、DBを変更し、'/'へリダイレクト
-post '/memos/:id/edit' do
-  @id = params[:id].to_i
-  settings.md.edit(@id, params[:title], params[:content])
+# 各メモの edit 表示後、result_hash を変更し、'/'へリダイレクト
+put '/memos/:memo_id' do
+  @memo_id = params[:memo_id].to_i
+  # 配列の対象ハッシュを変更する
+  settings.result_hash[@memo_id][:title] = params[:title]
+  settings.result_hash[@memo_id][:body] = params[:body]
   redirect '/'
 end
 
