467492a33f94c1adb0f6306f7b3c58a7a10d0db1
keikosmile <69710419+keikosmile@users.noreply.github.com>
2021-08-17 00:29:30 +0900

    table_click.js をjQueryを使わないようにして簡略化。

--- public/js/table_click.js
+++ public/js/table_click.js
@@ -1,12 +1,5 @@
-$(function() {
-  $('.js-table__row-link[data-href]').addClass('clickable').click(
-    function() {
-      window.location = $(this).attr('data-href');
-  }).find('a').hover( function() {
-    $(this).parents('tr').unbind('click');
-  }, function() {
-    $(this).parents('tr').click( function() {
-      window.location = $(this).attr('data-href');
-    });
-  });
+document.querySelectorAll('.js-table__row-link').forEach( row => {
+  row.addEventListener('click', () => {
+    location = row.getAttribute('data-href');
+  })
 });
