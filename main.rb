eeee27eadf55e9c37519d57282c70784c4fc0c56
keikosmile <69710419+keikosmile@users.noreply.github.com>
2021-08-18 13:21:33 +0900

    index.rb の read_data を read_memos に、write_data を write_memos に変更。

--- main.rb
+++ main.rb
@@ -9,43 +9,43 @@ class MemoDB
   JSON_FILE = './database.json'
 
   class << self
-    def read_data
+    def read_memos
       memos = {}
       memos = JSON.parse(File.read(JSON_FILE)) unless File.zero?(JSON_FILE)
       memos
     end
 
-    def write_data(memos)
+    def write_memos(memos)
       File.open(JSON_FILE, 'w') do |file|
         JSON.dump(memos, file)
       end
     end
 
     def select(memo_id)
-      memos = MemoDB.read_data
+      memos = MemoDB.read_memos
       memos[memo_id]
     end
 
     def select_all
-      MemoDB.read_data
+      MemoDB.read_memos
     end
 
     def insert(title, body)
-      memos = MemoDB.read_data
+      memos = MemoDB.read_memos
       memos[SecureRandom.uuid] = { 'title' => title, 'body' => body }
-      MemoDB.write_data(memos)
+      MemoDB.write_memos(memos)
     end
 
     def delete(memo_id)
-      memos = MemoDB.read_data
+      memos = MemoDB.read_memos
       memos.delete(memo_id)
-      MemoDB.write_data(memos)
+      MemoDB.write_memos(memos)
     end
 
     def update(memo_id, title, body)
-      memos = MemoDB.read_data
+      memos = MemoDB.read_memos
       memos[memo_id] = { 'title' => title, 'body' => body }
-      MemoDB.write_data(memos)
+      MemoDB.write_memos(memos)
     end
   end
 end
